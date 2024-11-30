1635568a2d070f50b481e5ed56f4591ff5de69db
SDYTO <100180040+SDYTO@users.noreply.github.com>
2024-11-30 15:28:56 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ const data = reactive({
     '当你在凝视着网页的时候<br/>网页也正在凝视着你',
   ],
   navLinks: [{
-    name: '博 客',
+    name: '博33333333 客',
     link: 'https://blog.sunguoqi.com',
   }, {
     name: '影 集',
